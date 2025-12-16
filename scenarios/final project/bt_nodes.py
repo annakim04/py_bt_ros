8f5a7b651556daaa693aaeede34d648e9a0b17f8
pielee <imdaniel01@naver.com>
2025-12-17 01:04:33 +0900

    Update coordinates for charge, pickup, and wait

--- scenarios/final project/bt_nodes.py
+++ scenarios/final project/bt_nodes.py
@@ -74,9 +74,9 @@ def _create_nav_goal(node, x, y, yaw=None, pose_stamped=None):
 # =========================================================
 # Coordinates
 # =========================================================
-CHARGE_X, CHARGE_Y, CHARGE_YAW = -4.198, 0.200, deg(0.0)
-PICKUP_X, PICKUP_Y, PICKUP_YAW = -6.326, 3.209, deg(90.0)
-WAIT_X, WAIT_Y, WAIT_YAW = -4.198, 0.200, deg(0.0)
+CHARGE_X, CHARGE_Y, CHARGE_YAW = 1.929, 0.039, deg(0.0)
+PICKUP_X, PICKUP_Y, PICKUP_YAW = 2.673, 4.222, deg(90.0)
+WAIT_X, WAIT_Y, WAIT_YAW = 0.340, 0.163, deg(0.0)
 
 NAV_ACTION_NAME = "/limo/navigate_to_pose"
 
