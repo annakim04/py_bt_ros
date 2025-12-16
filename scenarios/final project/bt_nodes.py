8a88fa5ba07033d150b43854eefccb1539d2b4a6
pielee <imdaniel01@naver.com>
2025-12-17 01:58:39 +0900

    Update bt_nodes.py

--- scenarios/final project/bt_nodes.py
+++ scenarios/final project/bt_nodes.py
@@ -76,7 +76,7 @@ def _create_nav_goal(node, x, y, yaw=None, pose_stamped=None):
 # =========================================================
 CHARGE_X, CHARGE_Y, CHARGE_YAW = 1.929, 0.039, deg(-82.5)
 PICKUP_X, PICKUP_Y, PICKUP_YAW = 2.673, 4.222, deg(-67.9)
-WAIT_X, WAIT_Y, WAIT_YAW = 0.340, 0.163, deg(0.0)
+WAIT_X, WAIT_Y, WAIT_YAW = 0.340, 0.163, deg(-7.9)
 
 NAV_ACTION_NAME = "/limo/navigate_to_pose"
 
